71801c622b8b3d6fd9fefd1473a8fe8b4b751ff7
Rohan <73487901+rohanvemu@users.noreply.github.com>
2024-08-27 14:39:45 -0500

    Update riceoedk.py

--- riceoedk.py
+++ riceoedk.py
@@ -1,7 +1,4 @@
-import time  # to simulate a real time data, time loop
-import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
 import streamlit as st  #
 import os
 
